92c82c7516c7b0e7819846b35fe1c5dfcbe99a74
FrozenAngels <583663275@qq.com>
2018-03-22 19:39:59 +0800

    Update run.sh
    adding countdown utilities

--- run.sh
+++ run.sh
@@ -1,8 +1,24 @@
 clear
+seconds_left=10  
+    echo "服务器将于${seconds_left}秒后启动，按ctrl+c可以退出……"  
+    while [ $seconds_left -gt 0 ];do  
+      echo -n $seconds_left  
+      sleep 1  
+      seconds_left=$(($seconds_left - 1))  
+      echo -ne "\r     \r" #清除本行文字  
+    done  
 java -Xincgc -Xmx4096M -Dfml.ignorePatchDiscrepancies=true -XX:+AggressiveOpts -XX:+UseCompressedOops  -jar KCauldron-1.7.10-1517.168-server.jar  -nogui
 while :; do echo
   read -p "Server closed.Restart? [y/n]: " Ifrestart
   if [[ ${Ifrestart} == 'y' ]]; then
+  seconds_left=10  
+    echo "服务器将于${seconds_left}秒后启动，按ctrl+c可以退出……"  
+    while [ $seconds_left -gt 0 ];do  
+      echo -n $seconds_left  
+      sleep 1  
+      seconds_left=$(($seconds_left - 1))  
+      echo -ne "\r     \r" #清除本行文字  
+    done  
 java -Xincgc -Xmx4096M -Dfml.ignorePatchDiscrepancies=true -XX:+AggressiveOpts -XX:+UseCompressedOops  -jar KCauldron-1.7.10-1517.168-server.jar  -nogui
 elif [[ ${Ifrestart} == 'n' ]]; then
 break

783a386f07272e71f604d882e5a4d5fad8c86b28
Hakurui_Reimu <583663275@qq.com>
2018-04-09 20:51:01 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ seconds_left=10
       seconds_left=$(($seconds_left - 1))  
       echo -ne "\r     \r" #清除本行文字  
     done  
-java -Xincgc -Xmx4096M -Dfml.ignorePatchDiscrepancies=true -XX:+AggressiveOpts -XX:+UseCompressedOops  -jar KCauldron-1.7.10-1517.168-server.jar  -nogui
+java -Xincgc -Xmx4096M -Dfml.ignorePatchDiscrepancies=true -XX:+AggressiveOpts -XX:+UseCompressedOops  -jar $var  -nogui
 while :; do echo
   read -p "Server closed.Restart? [y/n]: " Ifrestart
   if [[ ${Ifrestart} == 'y' ]]; then
